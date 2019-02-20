aa6d4512db9674ce4a77902530dcc4ab2bcda682
Brian DiRito <bdirito91@gmail.com>
2019-02-20 13:02:46 -0500

    added notes for easiest JS min_abs_dif solution

--- minimum_absolute_difference/easiest.js
+++ minimum_absolute_difference/easiest.js
@@ -1,13 +1,22 @@
 function minimumAbsoluteDifference(arr) {
+    // sort the array by value
     arr.sort((a, b) => { return a - b })
-    let minDif = Math.abs(arr[0] - arr[1])
-    let currentDif
 
+    // declare first minimum difference
+    // let first difference be minimum difference to use for comparison
+    let minDif = Math.abs(arr[0] - arr[1])
+
+    // iterate through the array
     for (let i = 1; i < arr.length - 1; i++){
-        currentDif = Math.abs(arr[i] - arr[i + 1])
+        // declare current difference
+        // value is absolute difference between current and next index
+        let currentDif = Math.abs(arr[i] - arr[i + 1])
+
         if (currentDif < minDif) {
+            // if current difference is less than minimum difference, replace
             minDif = currentDif
         }
     }
+    // return smallest difference
     return minDif
 }
