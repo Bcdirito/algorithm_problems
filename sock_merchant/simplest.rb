0f696d7db7f502bc797c664221b98224dc8e3dba
Brian DiRito <bdirito91@gmail.com>
2019-02-24 23:34:33 -0500

    added guide for basic.rb sock_merchant and edits for simplest.rb

--- sock_merchant/simplest.rb
+++ sock_merchant/simplest.rb
@@ -12,10 +12,10 @@ def sockMerchant(ar)
     # for each sock you encounter, create a key and/or increment the value by 1
     ar.each {|i| sockHash[i] += 1}
 
-    # iterate through the hash
+    # iterate through the hash values
     # using a ternary, if the value of a key is two or more, divide that value by two, then round down to get number of pairs
-    # if only one sock, move on to next key/value pair
-    sockHash.each {|k, v| v >= 2 ? total += (v / 2).floor : next}
+    # if only one sock, move on to next key-value pair
+    sockHash.values.each {|k, v| v >= 2 ? total += (v / 2).floor : next}
 
     # implicit return
     total
