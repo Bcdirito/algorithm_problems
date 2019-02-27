3cbf69cc9c6ce23c39d77a8da13aeedb5cdbd01a
Brian DiRito <bdirito91@gmail.com>
2019-02-27 10:29:19 -0500

    added standard JS and readme for two_sum

---
+++ two_sum/standard.js
@@ -0,0 +1,11 @@
+function two_sum(ints, s){
+    let history = {}
+    for (let i = 0; i < ints.length; i++){
+      if (history[ints[i]] !== undefined){
+        return [history[ints[i]], ints[i]]
+      } else {
+        let difference = s - ints[i]
+        history[difference] = ints[i]
+      }
+    }
+}
