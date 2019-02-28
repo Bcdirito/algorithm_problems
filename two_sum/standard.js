9961fc8b12788382762c1170e2d83bcc3cf0f957
Brian DiRito <bdirito91@gmail.com>
2019-02-28 11:06:16 -0500

    guide for math_issues and fixed guide for standard two_sum

--- two_sum/standard.js
+++ two_sum/standard.js
@@ -7,7 +7,7 @@ function two_sum(ints, sum){
     for (let i = 0; i < ints.length; i++){
       if (history[ints[i]] !== undefined){
         // if an integer key is found a pair exists
-        // return an array with the original key first, and second key second
+        // return an array with the original key first, and current index as the second index
         return [history[ints[i]], ints[i]]
       } else {
         // if no key of index exists, find the difference between the sum and index
