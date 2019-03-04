8434b20781ef274128aa8da145b9279f10097b8e
Brian DiRito <bdirito91@gmail.com>
2019-03-04 10:52:49 -0500

    added notes for basic.rb median two arrays

--- median_two_sorted_arrays/basic.rb
+++ median_two_sorted_arrays/basic.rb
@@ -1,11 +1,25 @@
+# Runtime 68ms, Memory Usage 12.8 MB
+
 def find_median_sorted_arrays(nums1, nums2)
+    # create a new array combining the two argument arrays
     arr = nums1 + nums2
+
+    # sort the new array
     arr.sort!
-    midInd = (arr.length / 2).floor
+
+    # get the middle index by dividing the length by two
+    # due to ruby syntax, this will automatically round down
+    midInd = arr.length / 2
+
     if arr.length % 2 != 0
+        # if the length is odd, we only need to return the middle index
         return arr[midInd]
     else
-        median = (arr[midInd].to_f + arr[midInd-1].to_f)/2.0
-        median
+        # if the length is even we need to return the average of the middle two indices
+        # add both indices together
+        # since ruby automatically defaults to rounding fixed numbers down, we must convert our two middle indices to floats
+        # once we have a sum, divide by a float so we get any remainder decimal value
+        # return the value
+        return (arr[midInd].to_f + arr[midInd-1].to_f)/2.0
     end 
 end
