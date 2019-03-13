9b57aa65a332b7583bc5b8eb13355496e017da52
Brian DiRito <bdirito91@gmail.com>
2019-03-12 20:35:30 -0400

    added guide simple.rb stock

--- best_time_to_buy_and_sell_stock/simple.rb
+++ best_time_to_buy_and_sell_stock/simple.rb
@@ -1,19 +1,30 @@
-def max_profit(prices)
-    if prices.length > 0
-        share = prices[0]
-        profit = 0
-        remainder_prices = prices[1..-1]
-        remainder_prices.each do |stock|
-           if stock < share
-               share = stock
-           end
+# Runtime 40 ms, Memory Usage 10.2 MB
 
-            if stock - share > profit
-                profit = stock - share
-            end
+def max_profit(prices)
+    # set the first share as equal to the first element in the array
+    # we will use this share as a checking point
+    share = prices[0]
+
+    # set a base profit equal to zero
+    profit = 0
+
+    # get the remainder elements of the array
+    # make sure to use .. so that the range is inclusive
+    remainder_prices = prices[1..-1]
+
+    # iterate through the remainder array
+    remainder_prices.each do |stock|
+        if stock < share
+            # if the current stock is less than the saved share, store the new stock
+            share = stock
         end
-        profit
-    else
-        return 0    
+
+        if stock - share > profit
+            # if the current stock minus the stored share is a bigger profit than the stored profit, make store that difference as the new profit
+            profit = stock - share
+        end
     end
+
+    # return the profit
+    profit
 end
