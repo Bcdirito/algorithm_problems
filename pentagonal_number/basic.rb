b1e7999c096df1c772f94e8727f2c646eb45aa71
Brian DiRito <bdirito91@gmail.com>
2019-03-15 13:00:14 -0400

    added guide basic.rb pentagonal_number

--- pentagonal_number/basic.rb
+++ pentagonal_number/basic.rb
@@ -1,12 +1,24 @@
 def PentagonalNumber(num)
-
+    # declare variable i
+    # i will be equal the number of dots
+    # i is declared with a value of one
     i = 1
-        if i < num
-            l = 2
-            until l > num
-                  i += (l - 1) * 5
-                  l += 1
-          end
-      end
-      i
+
+    # if i (or 1) is less than the input value
+    if i < num
+        # set l equal to one
+        l = 1
+        # until l is equal to the input number
+        until l == num
+            # the amount of dots increases by multiples of five
+            # increase i by that multiple
+            i += l * 5
+
+            # increase l
+            l += 1
+        end
+    end
+
+    # return i
+    i
   end
