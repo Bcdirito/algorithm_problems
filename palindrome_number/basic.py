a4965da1e66f25ff322197043f3668b6ad4be444
Brian DiRito <bdirito91@gmail.com>
2019-03-20 22:14:29 -0400

    added guide basic.py palindrome_number

--- palindrome_number/basic.py
+++ palindrome_number/basic.py
@@ -1,14 +1,18 @@
+# Runtime 136 ms, Memory Usage 10.9 MB
+
 class Solution(object):
-    def isPalindrome(self, x):
-        """
-        :type x: int
-        :rtype: bool
-        """
-        
+    def isPalindrome(self, x):  
+        # convert x into a string 
         str_int = str(x)
+
+        # declare an empty string variable
+        # this will become our reversed integer
         rev_str = ""
         
+        # iterate through the string
         for n in str_int:
+            # add characters to the beginning of the string to reverse it
             rev_str = n + rev_str
         
+        # return the boolean that comes from checking the equality of the two strings
         return rev_str == str_int
