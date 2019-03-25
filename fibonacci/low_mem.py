8529f95dd7ee995eb4ff2bef132201b5c4a5bb2e
Brian DiRito <bdirito91@gmail.com>
2019-03-25 14:20:49 -0400

    added guide low_mem.py fibonacci

--- fibonacci/low_mem.py
+++ fibonacci/low_mem.py
@@ -1,14 +1,24 @@
+# Runtime 20 ms, Memory Usage 11.8 MB
+
 class Solution(object):
     def fib(self, N):
-        if N > 0:
-            store = [0, 1]
-            i = 1
-            
-            while i < N:
-                sum = store[0] + store[1]
-                store = [store[-1], sum]
-                i += 1
-                
-            return store[-1]
-        else:
-            return 0
+        # declare a store list that contains the first two numbers in the sequence
+        store = [0, 1]
+
+        # declare a counter variable, set it equal to run
+        i = 1
+        
+        # run the loop while i is less than the input N
+
+        while i < N:
+            # declare a variable equal to the sum of the two integers in the store
+            sum = store[0] + store[-1]
+
+            # to keep memory usage low, replace the elements in the store list with the last value in the previous store, and the sum
+            store = [store[-1], sum]
+
+            # increment the counter by one
+            i += 1
+
+        # return the last value in the store  
+        return store[-1]
