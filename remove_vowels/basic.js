c0a035aece98e0807f117952c9948cff5389b874
Brian DiRito <bdirito91@gmail.com>
2019-04-04 11:38:52 -0400

    added guide basic.js

--- remove_vowels/basic.js
+++ remove_vowels/basic.js
@@ -1,15 +1,24 @@
 function disemvowel(str) {
+    // declare a variable with a value of an empty string 
+    // this will contain our edited string
     let editStr = ""
+
+    // declare an array containing all of the vowels
     let vowels = ["a", "e", "i", "o", "u"]
     
+    // iterate through the string
     for (let i = 0; i < str.length; i++){
-      let lowerCase = str[i].toLowerCase()
-      if (vowels.includes(lowerCase)){
-        continue
-      } else {
-        editStr += str[i]
-      }
+        // get the lowercased character and declare it to a variable
+        let lowerCase = str[i].toLowerCase()
+        if (vowels.includes(lowerCase)){
+            // if the vowels array contains the the lowerCase variable continue to the next index
+            continue
+        } else {
+            // if not, add it to the editStr variable
+            editStr += str[i]
+        }
     }
     
+    // return the edited string
     return editStr
   }
