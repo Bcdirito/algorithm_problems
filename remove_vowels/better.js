6c5092daf1d66371ee30b95af34a1388f7bb9300
Brian DiRito <bdirito91@gmail.com>
2019-04-04 11:30:15 -0400

    added better.js guide remove vowels

--- remove_vowels/better.js
+++ remove_vowels/better.js
@@ -1,5 +1,9 @@
 function disemvowel(str) {
+    // declare variable equal to an empty string
+    // this is what we will put our edited string into
     let editStr = ""
+
+    // declare an object containing the vowels
     vowelObj = {
       "a": 0,
       "e": 0,
@@ -8,14 +12,18 @@ function disemvowel(str) {
       "u": 0
     }
     
+    // iterate through the string
     for (let i = 0; i < str.length; i++){
-      let lowerCase = str[i].toLowerCase()
-      if (vowelObj[lowerCase] !== undefined){
-        continue
-      } else {
-        editStr += str[i]
-      }
+        // check to see if the contains the lowercased character
+        if (vowelObj[str[i].toLowerCase()] !== undefined){
+            // if the obj contains the lowercased character continue to the next index
+            continue
+        } else {
+            // if it doesn't add that character to the editStr variable
+            editStr += str[i]
+        }
     }
 
+    // return the edited variable
     return editStr
 }
