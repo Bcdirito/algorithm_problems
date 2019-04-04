0d317efb82242cce073a508d332c2dd30add8601
Brian DiRito <bdirito91@gmail.com>
2019-04-04 11:10:43 -0400

    added guide basic.py remove vowels

--- remove_vowels/basic.py
+++ remove_vowels/basic.py
@@ -1,8 +1,15 @@
 def disemvowel(string):
+    # delcare a variable with a value of an empty string
+    # this will eventually be our edited string
     edit_string = ""
+
+    # delcare a list containing the vowels
     vowels = ["a", "e", "i", "o", "u"]
     
+    # iterate through the string
     for s in string:
+        # check if the lowercased character is contained in the vowels list
+        # if not add it to the the edit_string variable
         if s.lower() not in vowels:
             edit_string += s
     
