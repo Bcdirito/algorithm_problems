36bf41b96298d46df475876189bc9107cd5e88a4
Brian DiRito <bdirito91@gmail.com>
2019-04-16 14:31:51 -0400

    added basic.rb guide reverse and digitize number

--- reverse_and_digitize_number/basic.rb
+++ reverse_and_digitize_number/basic.rb
@@ -1,11 +1,21 @@
 def digitize(n)
+    # convert the number to a string
+    # split the string into an array
     split_str = n.to_s.split("")
+
+    # declare an empty array variable
     int_arr = []
-    
+
+
+    # iterate through the string array
     split_str.each do |i|
-      num = i.to_i
-      int_arr.unshift(num)
+        # convert each index to an integer
+        num = i.to_i
+
+        # add the integer to the beginning of the array
+        int_arr.unshift(num)
     end
     
-    int_arr
+    # return the array of numbers
+    return int_arr
 end
