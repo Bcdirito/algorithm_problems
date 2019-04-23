64fb195d664fe685f6448150ad71646410f0ee50
Brian DiRito <bdirito91@gmail.com>
2019-04-22 21:10:05 -0400

    added guides for highest_scoring_word

--- highest_scoring_word/basic.js
+++ highest_scoring_word/basic.js
@@ -1,25 +1,49 @@
 function high(x){
-  let splitStr = x.split(" ")
-  let wordObj = {}
-  const checker = "a".charCodeAt() - 1
-  let highestWord;
-  let highestWordVal = 0
-  
-  for (let i = 0; i < splitStr.length; i++){
-    let word = splitStr[i]
-    wordObj[word] = 0
-    for (let l = 0; l < word.length; l++) {
-      wordObj[splitStr[i]] += word[l].charCodeAt() - checker
+    // split the string by word into an array
+    let splitStr = x.split(" ")
+
+    // declare an empty word object
+    let wordObj = {}
+
+    // declare a checker variable equal to one less than the ascii of a
+    // this is equal to 96
+    const checker = "a".charCodeAt() - 1
+
+    // declare an empty string variable that will house the highest word
+    let highestWord = ""
+
+    // declare an highest word value variable and set it equal to 0
+    // this will be used to compare the values of each word
+    let highestWordVal = 0
+    
+    // iterate through the split string array
+    for (let i = 0; i < splitStr.length; i++){
+        // declare a variable that will hold the word
+        // this was done for easier writing
+        let word = splitStr[i]
+
+        // add a key/value pair of each word
+        // declare it's value to 0
+        wordObj[word] = 0
+
+        // iterate through each character in the word
+        for (let l = 0; l < word.length; l++) {
+            // add the difference of the character and the checker to the word key
+            wordObj[splitStr[i]] += word[l].charCodeAt() - checker
+        }
     }
-  }
-  
-  for (const key in wordObj){
-    if (wordObj[key] > highestWordVal){
-      highestWord = key
-      highestWordVal = wordObj[key]
+    
+    // iterate through the key value pairs of the object
+    for (const key in wordObj){
+        // check if the current key's value is higher than the highest word value variable
+        if (wordObj[key] > highestWordVal){
+            // if so, set the highestWord to the current key
+            // and set the highestWordVal variable to the current value
+            highestWord = key
+            highestWordVal = wordObj[key]
+        }
     }
-  }
-  
-  if (highestWord === undefined) return ""
-  else return highestWord
+    
+    // return the highestWord variable
+    return highestWord
 }
--- highest_scoring_word/basic.py
+++ highest_scoring_word/basic.py
@@ -1,19 +1,41 @@
 def high(x):
+    # declare a checker variable that is one less than the ascii value of a
+    # this is equal to 96
     checker = ord("a") - 1
+
+    # declare an empty word dictionary
+    # we will store our words in this dictionary
     word_dict = {}
+
+    # split the string by words for iteration later
     split_str = x.split()
+
+    # declare a value variable for comparing word values
     highest_word_val = 0
+
+    # declare an empty string variable
+    # this will store the highest word
     highest_word = ""
     
-    
+    # iterate through the words in our split_str list
     for word in split_str:
+        # add a key of the current to the word_dictionary
+        # declare its value to 0
         word_dict[word] = 0
+
+        # iterate through the word
         for char in word:
+            # add the difference of the ascii code minues the character to the word key
             word_dict[word] += ord(char) - checker
-            
+
+    # iterate through the keys in the word_dict dictionary    
     for key in word_dict:
+        # check if the current word is higher than the value of the highest word
         if word_dict[key] > highest_word_val:
+            # if it is, set the value of the current word to the highest_word_val variable
+            # and set the highest_word equal to the key, or current word
             highest_word_val = word_dict[key]
             highest_word = key
     
+    # return the highest word
     return highest_word
