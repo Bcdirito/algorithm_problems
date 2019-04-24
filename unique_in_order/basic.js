bb93e456be60c1f799a986e6cdb93dd2565cf530
Brian DiRito <bdirito91@gmail.com>
2019-04-24 12:39:11 -0400

    added guides for unique in order

--- unique_in_order/basic.js
+++ unique_in_order/basic.js
@@ -1,9 +1,13 @@
 function uniqueInOrder(iterable){
-  let charArr = []
-  
-  for (let i = 0; i < iterable.length; i++){
-    if (iterable[i] !== iterable[i + 1]) charArr.push(iterable[i])
-  }
-  
-  return charArr
+    // declare an empty array that will hold our unique order characters
+    let charArr = []
+    
+    // iterate through the iterable
+    for (let i = 0; i < iterable.length; i++){
+        // if the current character does not equal the next character, push it into the array
+        if (iterable[i] !== iterable[i + 1]) charArr.push(iterable[i])
+    }
+    
+    // return the array
+    return charArr
 }
