6c30641a858ca5677aa1c114d3ada1345951653e
Brian DiRito <bdirito91@gmail.com>
2019-04-24 13:03:06 -0400

    added guides for open or senior

--- open_or_senior/basic.rb
+++ open_or_senior/basic.rb
@@ -1,11 +1,19 @@
 def open_or_senior(data)
+    # declare an empty array to contain "Senior" & "Open" values
     final_arr = []
+
+    # iterate through the data
     data.each do |a|
-      if a[0] >= 55 && a[1] > 7
-        final_arr << "Senior"
-      else
-        final_arr << "Open"
-      end
+        if a[0] >= 55 && a[1] > 7
+            # if the age is 55 or older and the handicap is above 6
+            # push "Senior" into the final_arr
+            final_arr << "Senior"
+        else
+            # if not, push "Open" into the array
+            final_arr << "Open"
+        end
     end
-    return final_arr
+    
+    # return the final_arr
+    final_arr
   end
