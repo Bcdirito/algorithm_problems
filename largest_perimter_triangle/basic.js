519490bd2b739f84fc582d4f6bf1733130ed525e
Brian DiRito <bdirito91@gmail.com>
2019-04-30 11:09:14 -0400

    added guide largest_perimieter_triangle

--- largest_perimter_triangle/basic.js
+++ largest_perimter_triangle/basic.js
@@ -1,17 +1,27 @@
+// Runtime 104 ms, Memory Usage 38.4 MB
+
 function largestPerimeter(A) {
-    let sortArr = A.sort(function(a, b){return a-b})
-    let finalAmt = 0
-    let sum;
+    // sort a copy of the original Array
+    let sortArr = A.slice(0).sort((a, b) => {return a-b})
+
+    // declare a perimeter variable with a value of 0
+    // like two sum, this will be used to compare perimeter lengths
+    let largestPerim = 0
     
     for (let i = 0; i < sortArr.length - 2; i++){
+        // check if the three elements can make a valid triangle
         if (sortArr[i] + sortArr[i+1] > sortArr[i + 2]){
-            sum = sortArr[i] + sortArr[i+1] + sortArr[i + 2]
-        }
-        
-        if (sum > finalAmt){
-            finalAmt = sum
+            // get the perimeter of the triangle
+            let sum = sortArr[i] + sortArr[i+1] + sortArr[i + 2]
+
+            // compare the perimeter of the current triangle to the largestPerim variable
+            if (sum > largestPerim){
+                // if it is, make the largestPerim variable equal to the current triangle value
+                largestPerim = sum
+            }
         }
     } 
     
-    return finalAmt
+    // return the largestPerim
+    return largestPerim
 };
