65f7f3b95b93e4b76d698dfa6da5a9d654024182
Brian DiRito <bdirito91@gmail.com>
2019-07-27 14:35:57 -0400

    revised JS solution and added guide

--- jewels_and_stones/basic.js
+++ jewels_and_stones/basic.js
@@ -1,11 +1,18 @@
+// Runtime 48 ms, Memory Usage 33.7 MB
+
 const numJewelsInStones = (J, S) => {
-    let jewelArr = J.split("")
+    // Declare variable equal to zero
+    // This will be the amount of shared stones
     let stoneAmt = 0
     
+    // iterate through the larger string
     for (let i = 0; i < S.length; i++){
-        if (jewelArr.includes(S[i])){
-            stoneAmt += 1
-        }
+        // check if the current index is included in the J string
+        // if so, increment the total by 1
+
+        if (J.includes(S[i])) stoneAmt += 1   
     }
+
+    // return the total
     return stoneAmt
 };
