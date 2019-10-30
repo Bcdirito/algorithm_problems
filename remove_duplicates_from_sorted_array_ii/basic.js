a77d28c6823c3d907e14292c2f77133a19ef8ab0
Bcdirito <bdirito91@gmail.com>
2019-10-30 15:03:57 -0400

    added guide basic.js

--- remove_duplicates_from_sorted_array_ii/basic.js
+++ remove_duplicates_from_sorted_array_ii/basic.js
@@ -1,8 +1,22 @@
+// Runtime 60 ms,  Memory Usage 35.7 MB
+
 const removeDuplicates = (nums) => {
-    for (let i = 0; i < nums.length - 1; i++){
+
+    // iterate through the input array
+    for (let i = 0; i < nums.length - 2; i++){
+
+        // check if this element, the next element, and the following are the same number
         if (nums[i] === nums[i+1] && nums[i+1] === nums[i+2]){
-            nums.splice(i+1,1)
+
+            // if so, remove the current element from the array
+            nums.splice(i,1)
+
+            // decrement i by 1
+            // this will cause the iteration to run on the current element
             i--
         }
     }
+
+    // return the length of the new nums array
+    return nums.length
 };
