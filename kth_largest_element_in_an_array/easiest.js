e94587d3fc94def17e925edc67374e5cc013d1ba
Bcdirito <bdirito91@gmail.com>
2019-11-04 11:50:44 -0500

    added guide easiest.js

--- kth_largest_element_in_an_array/easiest.js
+++ kth_largest_element_in_an_array/easiest.js
@@ -1,3 +1,8 @@
+// Runtime 60 ms, Memory Usage 36.1 MB
+
 const findKthLargest = function(nums, k) {
+    // use JS built-in function to sort the nums array
+    // find the kth largest by subtracting k from the length of the array using the difference as the index
+    // return that element
     return nums.sort((a, b) => {return a - b})[nums.length-k]
 };
