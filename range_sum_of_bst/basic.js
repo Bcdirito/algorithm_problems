11882d5be71fda36cf2d63a635257342d9648e51
Bcdirito <bdirito91@gmail.com>
2019-11-15 11:46:42 -0500

    added guide

--- range_sum_of_bst/basic.js
+++ range_sum_of_bst/basic.js
@@ -1,15 +1,27 @@
+// Runtime ms, Memory Usage MB
+
 const rangeSumBST = (root, L, R) => {
-    
+    // initialize a total variable
+    // this will be our range sum we ultimately return
     let total = 0
     
+    // declare a recursive helper function
+    // it takes in a leaf
     const getSum = (leaf) => {
+
+        // check if the leaf's value falls in the inclusive range
+        // if so, add it to the total
         if (leaf.val >= L && leaf.val <= R) total += leaf.val
         
+        // check if each leaf has a left and/or right leaf
+        // if so, recurisvely run the function on that leaf
         if (leaf.left !== null) getSum(leaf.left)
         if (leaf.right !== null) getSum(leaf.right)
     }
     
+    // run the helper function starting at the root
     getSum(root)
     
+    // return the total
     return total
 };
