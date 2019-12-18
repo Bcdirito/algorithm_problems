28fb1f0fbd8e4bebdb4f5638408981a286a9f974
Bcdirito <bdirito91@gmail.com>
2019-12-18 12:13:04 -0500

    added guide basic js

--- tree/maximum_depth_of_n_ary_tree/basic.js
+++ tree/maximum_depth_of_n_ary_tree/basic.js
@@ -1,18 +1,33 @@
+// Runtime 60ms, Memory Usage 37.3 MB
+
 const maxDepth = (root) => {
+    // check if the tree exists
+    // if not the answer is null so we may return the root
     if (root === null) return root
     
+    // declare a variable with a value of zero
+    // this variable will be our maximum depth, and we will use it to check depths
     let maxLevel = 0
     
+    // declare a helper function that takes in a node and a level
+    // this function will traverse our tree
     const traverseTree = (node, level) => {
-        if (level >= maxLevel) maxLevel = level
+        // check the current level against the maxLevel variable
+        // if it is more, make it the current level the value of the maxLevel variable
+        if (level > maxLevel) maxLevel = level
+
+        // check if the current node has any children
         if (node.children) {
+            // if it does, iterate through each child and recursively call the function again to find the maximum depth level
             for (let i = 0; i < node.children.length; i++){
                 traverseTree(node.children[i], level + 1)   
             }
         }
     }
     
+    // call the helper function
     traverseTree(root, 1)
     
+    // return the max level
     return maxLevel
 };
