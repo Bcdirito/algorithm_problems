a4c2cfb3c1aeb92cabf302ca83f5671152d9a53d
Bcdirito <bdirito91@gmail.com>
2019-12-30 11:19:31 -0500

    added guide and revised basic.js

--- math/subtract_product_and_sum_of_digits/basic.js
+++ math/subtract_product_and_sum_of_digits/basic.js
@@ -1,23 +1,17 @@
+// Runtime 52 ms, Memory Usage 33.8 MB
+
 const subtractProductAndSum = (n) => {
+    // convert the argument integer to a string
+    // split the string by character
+    // use .map() to iterate through the split array and return each element as a number
+    // save that array to a variable
     let numArr = String(n).split("").map(char => {
         return Number(char)
     })
     
-    const getProduct = (arr) => {
-        let product = 1
-        
-        for (let i = 0; i < arr.length; i++){
-            product *= arr[i]
-        }
-        
-        return product
-    }
-    
-    const getSum = (arr) => {
-        let reducer = (total, currentVal) => total + currentVal
-        
-        return arr.reduce(reducer)
-    }
-    
-    return getProduct(numArr) - getSum(numArr)
+    // use .reduce() to get the product and sum
+    // the callback for the product should be (total, currentVal) => total*currentVal which will iterate through the array and multiply all values together
+    // the callback for the sum should be (total, currentVal) => total+currentVal which will iterate through the array and add all values together
+    // then subtract the sum from the product and return that value
+    return numArr.reduce((total, currentVal) => total*currentVal) - numArr.reduce((total, currentVal) => total+currentVal)
 };
