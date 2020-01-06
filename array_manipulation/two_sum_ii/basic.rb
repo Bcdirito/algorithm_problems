3c0182ec575b16a267c800253817b09e8db4a1a5
Bcdirito <bdirito91@gmail.com>
2020-01-06 12:23:05 -0500

    added guide rb solution

--- array_manipulation/two_sum_ii/basic.rb
+++ array_manipulation/two_sum_ii/basic.rb
@@ -1,8 +1,20 @@
+# Runtime 36 ms, Memory Usage 9.6 MB
+
 def two_sum(numbers, target)
+    # intialize an empty hash variable
     numbers_hash = {}
     
-    numbers.each_with_index {|num, idx| numbers_hash[target-num] = idx+1}
+    # iterate through the numbers array utilizing .each_with_index
+    # if the number is less or equal to the target add a key-value pair
+    # the key should be the difference of the target and the current element
+    # the value should be the index incremented by one
+    # if the number is greater than the target, break out of the iteration, as there is no need to continue
+    numbers.each_with_index {|num, idx| num <= target ? numbers_hash[target-num] = idx+1 : break}
     
+    # iterate through the numbers array again utilizing .each_with_index
+    # check if the key exists in the hash
+    # if it does, there is a two sum solution
+    # return a sorted array of the key's value and the current index in the iteration
     numbers.each_with_index do |num, idx|
         if numbers_hash[num]
             return [numbers_hash[num], idx+1].sort
