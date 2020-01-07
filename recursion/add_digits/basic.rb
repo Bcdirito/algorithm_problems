451319348550dc56991f4aa534c6216e10b29882
Bcdirito <bdirito91@gmail.com>
2020-01-07 11:36:08 -0500

    added guide

--- recursion/add_digits/basic.rb
+++ recursion/add_digits/basic.rb
@@ -1,7 +1,19 @@
+# Runtime 40 ms, Memory Usage 9.7 MB
+
 def add_digits(num)
     
+    # define a helper method that takes in a number
     def add_recursive(number)
+        # first, convert the number into a string
+        # then split that string by each character
+        # use .map to iterate through the string
+        # convert each character to an integer and return it to the array
+        # then, utilizing .inject, add all of the integeres in the array to get your sum
         num_sum = number.to_s.split("").map {|char| char.to_i}.inject(0, :+)
+
+        # check if the sum is greater than 9
+        # if so, recursively run the function again
+        # if not, return the sum
         if num_sum > 9
             return add_recursive(num_sum)
         else
@@ -9,5 +21,6 @@ def add_digits(num)
         end
     end
     
+    # implicit retur
     add_recursive(num)
 end
