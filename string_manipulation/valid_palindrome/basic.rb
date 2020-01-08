7d2bfa5f67e7303aff23a21cdbf43db6d8d237f0
Bcdirito <bdirito91@gmail.com>
2020-01-08 12:50:51 -0500

    added guide rb solution

--- string_manipulation/valid_palindrome/basic.rb
+++ string_manipulation/valid_palindrome/basic.rb
@@ -1,17 +1,29 @@
+# Runtime 92 ms, Memory Usage 15 MB
+
 def is_palindrome(s)
+    # initialize an empy hash variable
     letter_hash = {}
     
-    "abcdefghijklmnopqrstuvwxyz0123456789".split("").each {|char| letter_hash[char] = ""}
+    # split a string of every alphanumeric character
+    # create a key value pair for each character, where the value is 0
+    "abcdefghijklmnopqrstuvwxyz0123456789".split("").each {|char| letter_hash[char] = 0}
     
-    filter_string = s.downcase.split("").select {|char| letter_hash[char]}
-    filter_string_first = filter_string[0...filter_string.length/2]
-    filter_string_second = filter_string[filter_string.length/2..-1]
-        
-    filter_string_first.each_with_index do |char, idx|
-        if char != filter_string_second[-(idx+1)]
+    # split the input string, then iterate through the string using .select
+    # filter out non alphanumeric characters by checking if they have a key in the hash variable
+    # only accept characters with keys
+    filter_arr = s.downcase.split("").select {|char| letter_hash[char]}
+    
+    # iterate through the first half of the filtered array, which you get by using the exclusive ... operator
+    # use .each_with_index so we have the index to iterate backward through second half
+    # check if characters are equal
+    # if not, return false
+    filter_arr[0...filter_string.length/2].each_with_index do |char, idx|
+        if char != filter_string[-(idx+1)]
             return false
         end
     end
     
+    # if you made it here, the strings are equal
+    # implicit return true
     true
 end
