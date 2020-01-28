753131da6810656e80c0223fdd50e4ffc962a4bd
Bcdirito <bdirito91@gmail.com>
2020-01-28 10:30:08 -0500

    added guide better.rb

--- array_manipulation/tribonacci/better.rb
+++ array_manipulation/tribonacci/better.rb
@@ -1,11 +1,20 @@
+# Runtime 24 ms, Memory Usage 9.2 MB
+
 def tribonacci(n)
+    # initialize a start tribonacci array
     trib_arr = [0, 1, 1]
-    start_occurrences = 2
     
-    (n-start_occurrences).times do
-        trib_arr << trib_arr[-1] + trib_arr[-2] + trib_arr[-3]
-        print(trib_arr)
+    # determine the amount of times to run this logic by subtracting the 2 from n
+    (n-2).times do
+        # generate a new tribonacci array consisting of (in order):
+        # the pen ultimate value
+        # the last value
+        # the sum of the three values
+        trib_arr = [trib_arr[-2], trib_arr[-1], (trib_arr[-1] + trib_arr[-2] + trib_arr[-3])]
     end
     
-    trib_arr[n]
+    # utizling a ternary operator, determine if n is greater than two
+    # if it is, we ran the .times logic, so return the last value
+    # if not, we never ran the logic, return the n-th index in the array
+    n > 2 ? trib_arr.last : trib_arr[n]
 end
