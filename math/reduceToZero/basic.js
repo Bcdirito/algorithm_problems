44eeab4cc2d7b598daa1003f549c83fb1fc07453
Brian DiRito <bdirito91@gmail.com>
2020-12-17 08:43:39 -0500

    added guide basic.js

--- math/reduceToZero/basic.js
+++ math/reduceToZero/basic.js
@@ -1,10 +1,21 @@
+// Runtime 76 ms, Memory Usage 38.5 MB
 const numberOfSteps = (num) => {
+
+    // declare a recursive helper function that takes in a number and the number of steps taken
     const reduceNumber = (n, steps) => {
+
+        // check if the number is even or odd
+        // if even, divide by two
+        // if odd, decrement by one
         if (n % 2 === 0) n /= 2
         else n -= 1
         
+        // check if zero has been reached and either return the number of steps or recursively run the function again
         return n === 0 ?  steps : reduceNumber(n, steps+1)
     }
     
-    return num > 0 ? reduceNumber(num, 1) : num
+    // check if the number is greater than zero
+    // if so, run the recursive function
+    // if not, return 0, the function doesn't need to run
+    return num > 0 ? reduceNumber(num, 1) : 0
 }
