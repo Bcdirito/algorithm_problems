ed88d3f6b1a09dcd45e6f30631d769e55d73165b
Brian DiRito <bdirito91@gmail.com>
2020-12-23 08:29:08 -0500

    added guide better.py

--- math/subtract_product_and_sum_of_digits/basic.py
+++ math/subtract_product_and_sum_of_digits/basic.py
@@ -1,10 +1,17 @@
+# Runtime 20 ms, Memory Usage 14.1 MB
 def subtractProductAndSum(n):
+    # declare two variables
+    # the first will be the product we increment (which will need a base of 1 or it will always return 0)
+    # the second will be the sum we increment
     digit_prod = 1
     digit_sum = 0
     
+    # iterate through a string of the input number
+    # convert each character back to an integer and increase both variables
     for s in str(n):
         num = int(s)
         digit_prod *= num
         digit_sum += num
     
+    # substract the sum from the product and return the result
     return digit_prod - digit_sum
