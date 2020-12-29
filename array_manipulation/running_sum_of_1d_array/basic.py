c3b66da7179f6570701b233d1621fa930dfe82fe
Brian DiRito <bdirito91@gmail.com>
2020-12-29 08:55:40 -0500

    added guide basic.py

--- array_manipulation/running_sum_of_1d_array/basic.py
+++ array_manipulation/running_sum_of_1d_array/basic.py
@@ -1,9 +1,17 @@
-def runningSum(self, nums: List[int]) -> List[int]:
+# Runtime 44 ms, Memory Usage 14.3 MB
+def runningSum(nums):
+    # declare two empty list variables
+    # the first is the list of our sums
+    # the second is a growing collection of nums, used to calculate the sum
     sums = []
     it_nums = []
     
+    # iterate through the input param
+    # append the current element to the collection of nums
+    # get the sum of the collection of nums and append it to the list of sums
     for n in nums:
         it_nums.append(n)
         sums.append(sum(it_nums))
-        
+    
+    # return the sums list
     return sums
