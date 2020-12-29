4ae3d883102a39561dffc2b3b71fca352c4a9020
Brian DiRito <bdirito91@gmail.com>
2020-12-28 21:33:07 -0500

    added basic.js guide

--- array_manipulation/running_sum_of_1d_array/basic.js
+++ array_manipulation/running_sum_of_1d_array/basic.js
@@ -1,12 +1,24 @@
+// Runtime 84 ms, Memory Usage 37.5 MB
 const runningSum = (nums) => {
+
+    // declare three variables
+    // 1. an array of the sums
+    // 2. an arrary of the original numbers to add together
+    // 3. a reducer function to get the sums
     let sums = []
     let itNums = []
     let reducer = (total, currentVal) => total+currentVal
     
+
+    // iterate through the input array
+    // push the original numbers into the appropriate array
+    // add the sum of each of those numbers to the sums array
     for (let i = 0; i < nums.length; i++) {
         itNums.push(nums[i])
         sums.push(itNums.reduce(reducer))
     }
-    
+
+
+    // return the sums array
     return sums
 };
