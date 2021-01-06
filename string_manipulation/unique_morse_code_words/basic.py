a5257fb3c8d9283338c710d94cab912f84b0cd2e
Brian DiRito <bdirito91@gmail.com>
2021-01-05 19:34:56 -0500

    added guide basic.py

--- string_manipulation/unique_morse_code_words/basic.py
+++ string_manipulation/unique_morse_code_words/basic.py
@@ -1,12 +1,20 @@
 def uniqueMorseRepresentations(words):
+    # declare a list variable that contains the morse characters
     morse_chars = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
     
+    # declare a string containing the alphabet
     alpha = "abcdefghijklmnopqrstuvwxyz"
     
+    # declare three variables
+    # 1. a dictionary that we'll use to translate the chracters to morse
+    # 2. a dictionary to hold the translated words
+    # 3. a list containing unique translated words
     morse_dict = {}
     translation_dict = {}
     translation_list = []
     
+    # declare a helper method that takes in a string and the translation dictionary
+    # this method will iterate through the string and translate it to morse
     def translate_word(word, dictionary):
         translation = ""
         
@@ -18,10 +26,15 @@ def uniqueMorseRepresentations(words):
     for c in range(len(morse_chars)):
         morse_dict[alpha[c]] = morse_chars[c]
     
+    # iterate through the list of words
+    # call the helper method to translate the word to morse
+    # check if the word is in the translation dictionary
+    # if not, add it to the dictionary and translation list
     for w in words:
         trans = translate_word(w, morse_dict)
         if trans not in translation_dict:
-            translation_dict[trans] = True
+            translation_dict[trans] = ""
             translation_list.append(trans)
     
-    return len(translation_dict)
+    # return the list of unique translated words
+    return len(translation_list)
