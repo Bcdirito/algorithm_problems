a0db3728d9857dd5387e7e1ab4959df5d690f708
Brian DiRito <bdirito91@gmail.com>
2021-01-14 20:23:47 -0500

    added giude

--- array_manipulation/find_duplicates/basic.js
+++ array_manipulation/find_duplicates/basic.js
@@ -1,6 +1,15 @@
+// Runtime 120ms, Memory Usage 49.8 MB
+
 const findDuplicates = (nums) => {
+    // declare an empty object
+    // this object will hold all unique values previously iterated through
     let history = {}
     
+    // call .filter() on the nums array
+    // check if the number exists in the history object
+    // if so, collect that number
+    // otherwise add it to the history
+    // return the filtered array
     return nums.filter(num => {
         if (history[num] !== undefined) return num
         else history[num] = 1
