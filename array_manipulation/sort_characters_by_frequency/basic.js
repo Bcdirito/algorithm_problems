84078e6fc39fab26b77089e46a7f70e8ce8f3361
Brian DiRito <bdirito91@gmail.com>
2021-01-18 19:50:31 -0500

    added guide basic.js

--- array_manipulation/sort_characters_by_frequency/basic.js
+++ array_manipulation/sort_characters_by_frequency/basic.js
@@ -1,23 +1,31 @@
+// Runtime 96 ms, Memory Usage 46.8 MB
 const frequencySort = (s) => {
+    // declare two variables
+    //  an emtpy object we will use for history
+    // an empty string that we will sort the characters by frequency
     let history = {}
     let sortStr = ""
     
+    // iterate through the input string to get the number of occurrences in the array
     for (let i = 0; i < s.length; i++) {
         if (history[s[i]] !== undefined) history[s[i]] += 1
         else history[s[i]] = 1
     }
     
+    // write a helper method to add the character the number of times it appears in the sorted string
     const addToStr = (el, count) => {
         for (let i = 0; i < count; i++) {
             sortStr += el
         } 
     }
     
-    const sortedKeys = Object.keys(history).sort((a, b) => {return history[b] - history[a]})
-    
-    for (const char of sortedKeys) {
+    // sort the keys of the history object by their occurrences in descending order
+    // iterate through the array
+    // call the helper method with each character and the number of times it occurs
+    for (const char of Object.keys(history).sort((a, b) => {return history[b] - history[a]})) {
         addToStr(char, history[char])
     }
     
+    // return the sorted string
     return sortStr
 };
