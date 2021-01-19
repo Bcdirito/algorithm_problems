9a2a54ba2d3ec59eaf73317a793fc3cdfc105d04
Brian DiRito <bdirito91@gmail.com>
2021-01-19 08:42:35 -0500

    revised basic.py

--- array_manipulation/sort_characters_by_frequency/basic.py
+++ array_manipulation/sort_characters_by_frequency/basic.py
@@ -1,4 +1,4 @@
-def frequencySort(self, s: str) -> str:
+def frequencySort(s: str):
     count_dict = {}
     sort_str = ''
     
@@ -6,11 +6,9 @@ def frequencySort(self, s: str) -> str:
         if x in count_dict:
             count_dict[x] += 1
         else:
-            count_dict[x] = 1
+            count_dict[x] = 1          
     
-    sorted_keys = sorted(count_dict.items(), key=lambda x: x[1], reverse=True)           
-    
-    for k in sorted_keys:
+    for k in sorted(count_dict.items(), key=lambda x: x[1], reverse=True):
         for i in range(0, k[1]):
             sort_str += k[0]
     
