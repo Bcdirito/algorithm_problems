6249da713d17c62ed94732b9aa4cd99b1f7c0c35
Brian DiRito <bdirito91@gmail.com>
2021-02-08 20:18:06 -0500

    added guide

--- string_manipulation/largest_substring_between_two_equal_characters/basic.js
+++ string_manipulation/largest_substring_between_two_equal_characters/basic.js
@@ -1,7 +1,14 @@
+// Runtime 80 ms, Memory Usage 40.2 MB
 const maxLengthBetweenEqualCharacters = (s) => {
+    // declare two variables
+    // declare a length variable with no value
+    // split the string by character
     let length;
     let splitS = s.split("")
     
+    // declare a helper function
+    // this function grabs the index of any characters matching the value
+    // this helps if there are more than two equal characters
     const getIdxs = (arr, val) => {
         let idxs = []
         
@@ -12,6 +19,8 @@ const maxLengthBetweenEqualCharacters = (s) => {
         return idxs
     }
     
+    // iterate through the split string
+    // if there are matching characters, get the greatest difference
     for (let i = 0; i < splitS.length - 1; i++) {
         let idxs = getIdxs(splitS, splitS[i])
         if (idxs.length > 0) {
@@ -20,5 +29,7 @@ const maxLengthBetweenEqualCharacters = (s) => {
         }
     }
     
+    // if no matching characters, return -1
+    // else return the greatest difference
     return length === undefined ? -1 : length
 };
