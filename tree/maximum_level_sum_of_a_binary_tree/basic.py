e72662957a4a810451478e4902c1e444d93f3c4a
Brian DiRito <bdirito91@gmail.com>
2021-02-18 18:46:44 -0500

    guide basic.py

--- tree/maximum_level_sum_of_a_binary_tree/basic.py
+++ tree/maximum_level_sum_of_a_binary_tree/basic.py
@@ -1,6 +1,9 @@
+# Runtime 292 ms, Memory Usage 18.9 MB
 def maxLevelSum(root):
-    level_dict = {}
-    
+    # declare a helper method that traverses the tree
+    # it takes in the leaf and the level
+    # key: level
+    # value: values of level
     def traverseTree(leaf, lvl):
         if lvl in level_dict:
             level_dict[lvl].append(leaf.val)
@@ -12,11 +15,21 @@ def maxLevelSum(root):
         if (leaf.right):
             traverseTree(leaf.right, lvl+1)
     
+    # declare a dictionary
+    level_dict = {}
+
+    # call the helper method
     traverseTree(root, 1)
     
+    # declare two variables
+    # one to hold the level of the max val
+    # a second to hold the max val
     max_level = 0
     max_val = -9999
     
+    # iterate through the dictionary
+    # compare sum against max val
+    # if greater, replace max level and val with sum
     for key in level_dict:
         level_sum = sum(level_dict[key])
         if level_sum > max_val:
