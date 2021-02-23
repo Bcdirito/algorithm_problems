4b9ccdc39c763dd0c9624c8e4d42969f35e21913
Brian DiRito <bdirito91@gmail.com>
2021-02-23 18:15:41 -0500

    added guide basic.py

--- tree/all_elements_in_two_binary_search_trees/basic.py
+++ tree/all_elements_in_two_binary_search_trees/basic.py
@@ -1,4 +1,8 @@
+# Runtime 300 ms, Memory Usage 22.5 MB
 def getAllElements(root1, root2):
+    
+    # declare a recursive helper method that takes in a node
+    # this method will traverse through the tree
     def traverse_tree(node):
         vals.append(node.val)
         
@@ -7,11 +11,14 @@ def getAllElements(root1, root2):
         if (node.right):
             traverse_tree(node.right)
     
+    # declare an empty list that will hold all of the values of the tree
     vals = []
     
+    # check if both trees exist and iterate through them
     if (root1):
         traverse_tree(root1)
     if (root2):
         traverse_tree(root2)
     
+    # return a sorted list of vals
     return list(sorted(vals))
