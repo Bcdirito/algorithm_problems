988ceaddb24d15397c540545e5b79cbfb171ee8b
Brian DiRito <bdirito91@gmail.com>
2021-02-22 19:52:15 -0500

    guide basic.js

--- tree/all_elements_in_two_binary_search_trees/basic.js
+++ tree/all_elements_in_two_binary_search_trees/basic.js
@@ -1,14 +1,19 @@
+// Runtime 264 ms, Memory Usage 55.7 MB
 const getAllElements = (root1, root2) => {
+    // declare a recursive function to traverse through the tree
     const traverseTree = (leaf) => {
         vals.push(leaf.val)
         if (leaf.left) traverseTree(leaf.left)
         if (leaf.right) traverseTree(leaf.right)
     }
     
+    // this variable will hold all of the values
     let vals = []
     
+    // traverse through both trees if they have values
     if (root1) traverseTree(root1)
     if (root2) traverseTree(root2)
     
+    // return the sorted
     return vals.sort((a, b) => {return a - b})
 };
