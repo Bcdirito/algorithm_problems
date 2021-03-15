80161a8ed508ffd794d530d7d1d4913a294827ff
Brian DiRito <bdirito91@gmail.com>
2021-03-15 18:34:38 -0400

    basic.js solution

--- array_manipulation/string_matching_in_an_array/basic.js
+++ array_manipulation/string_matching_in_an_array/basic.js
@@ -1,4 +1,8 @@
+// Runtime 80 ms, Memory Usage 40 MB
 const stringMatching = (words) => {    
+    // declare a helper function
+    // this function will iterate through the words and see if there are other words that include the substring
+    // if none are found, default to false
     const checkMatch = (arr, word) => {
         for (let i = 0; i < arr.length; i++) {
             if (arr[i].includes(word)) return true
@@ -7,6 +11,10 @@ const stringMatching = (words) => {
         return false
     }
     
+    // iterate through the input words using .filter
+    // make a copy of the input aray
+    // remove the current word
+    // run the helper function and keep the word if it is in another word
     return words.filter(word => {
         const copy = words.slice()
         copy.splice(words.indexOf(word), 1)
