98f04b9da718f52929f6f83ef2900f68e8c887ee
Brian DiRito <bdirito91@gmail.com>
2021-03-16 18:15:00 -0400

    basic.py guide

--- array_manipulation/string_matching_in_an_array/basic.py
+++ array_manipulation/string_matching_in_an_array/basic.py
@@ -1,6 +1,11 @@
+# Runtime 32ms, Memory Usage 14.3 MB
 def stringMatching(words):
+    # declare empty list variable to hold the valid substrings
     substrings = []
     
+    # iterate through the input list
+    # check if word exists in other string
+    # if so, push to list variable
     for w in words:
         word = w
         for x in words:
