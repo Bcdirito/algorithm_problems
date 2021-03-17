a8ccf3b2149b5f87170137b9b4ac2e09a7d6f098
Brian DiRito <bdirito91@gmail.com>
2021-03-17 17:41:54 -0400

    basic.js solution

--- array_manipulation/three_consecutive_odds/basic.js
+++ array_manipulation/three_consecutive_odds/basic.js
@@ -1,7 +1,15 @@
 // Runtime 68ms, Memory Usage 38.4 MB
 const threeConsecutiveOdds = (arr) => {
+    // iterate through the array
+    // stop iterating after three before the final element, 
+    // there will not be any more subarrays with a length of three
     for (let i = 0; i < arr.length - 2; i++) {
+        // check if the current element is divisible by two
         if (arr[i] % 2 !== 0) {
+            // if so, create a subarray with a length of three
+            // check if every number is divisible by two
+            // if so return true
+            // else, continue iterating
             if (arr.slice(i, i+3).every(num => num % 2 !== 0) === true) return true
             else continue
         }
